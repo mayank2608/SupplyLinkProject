00e5f386956fee8fbc162244489daaca8d272429
username <wecp-dummy@gmail.com>
2025-09-20 12:46:25 +0000

    Day 25 task

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './navbar/navbar.component';
 import { AuthModule } from '../auth/auth.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
+    NavBarComponent 
   ],
   imports: [
     CommonModule,
@@ -12,6 +15,7 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   exports: [
+    NavBarComponent
   ]
 })
 export class SharedModule {}
--- frontend/src/app/supplylink/components/warehousesample/warehousesample.component.ts
+++ frontend/src/app/supplylink/components/warehousesample/warehousesample.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { Warehouse } from '../../types/Warehouse';
 
 @Component({
   selector: 'app-warehousesample',
